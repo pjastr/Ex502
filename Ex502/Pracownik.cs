8bce81b5d891d382d9f3fe815234fdf727b6756b
Piotrek <@>
2017-11-02 13:23:22 +0100

    przesłanie plików

---
+++ Ex502/Pracownik.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex502
+{
+    class Pracownik:IComparable<Pracownik>
+    {
+        private int wynagrodzenie;
+        private string imie;
+
+        public int CompareTo(Pracownik other)
+        {
+            if (this.wynagrodzenie == other.wynagrodzenie)
+            {
+                return this.imie.CompareTo(other.imie);
+            }
+            return other.wynagrodzenie.CompareTo(this.wynagrodzenie);
+        }
+
+        public Pracownik(string imie, int wynagrodzenie)
+        {
+            this.imie = imie;
+            this.wynagrodzenie = wynagrodzenie;
+        }
+
+        public string Info()
+        {
+            return this.imie + " " + this.wynagrodzenie;
+        }
+
+    }
+}
---
+++ Ex502/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex502
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Pracownik> pracownicy = new List<Pracownik>();
+            pracownicy.Add(new Pracownik("Jan", 3000));
+            pracownicy.Add(new Pracownik("Anna", 2500));
+            pracownicy.Add(new Pracownik("Jan", 3200));
+            pracownicy.Add(new Pracownik("Zofia", 4100));
+            pracownicy.Add(new Pracownik("Barbara", 4100));
+            pracownicy.Sort();
+            foreach(var element in pracownicy)
+            {
+                Console.WriteLine(element.Info());
+            }
+            Console.ReadKey();
+        }
+    }
+}
